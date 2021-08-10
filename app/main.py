e2e5ab6e0aa871e68a06d58e9f7142108dbc5652
evergreenacorn <rasakjustone01.11@gmail.com>
2021-08-10 19:56:36 +0300

    Добавлены переменные(текущий .yml-файл, словарь с путями офферов) в главный модуль проекта

--- app/main.py
+++ app/main.py
@@ -2,5 +2,9 @@ from downloading import  run, Config
 # from config import Config
 
 
+current_file = None
+updated_urls_dict = {}
+
+
 if __name__ == '__main__':
-    run()
+    run(current_file, updated_urls_dict)

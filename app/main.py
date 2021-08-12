f531024351f10ad0c71548b8443b9eb504776e69
evergreenacorn <rasakjustone01.11@gmail.com>
2021-08-12 13:33:48 +0300

    Исправлены именования функция и передаваемые им аргументы.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
-from downloading import  run, Config, circle_run
+from downloading import Config, run, os, dt
 # from config import Config
 
 
-current_file = None
+archive_file = Config.GZ_FILE
+current_file = os.path.join(
+            Config.BASE_DIR,
+            'yml_for_parsing_{}.yml'.format(
+                dt.now().strftime("%m-%d-%Y_%H:%M")
+            ))
 updated_urls_dict = {}
 
 
-def run_program(current_file, updated_urls_dict):
-    circle_run(current_file, updated_urls_dict)
+def run_program(archive_file, yml_file, urls_dict, offers_count=1000):
+    run(archive_file, yml_file, urls_dict, offers_count)
 
 
 if __name__ == '__main__':
-    run_program(current_file, updated_urls_dict)
+    run_program(archive_file, current_file, updated_urls_dict)

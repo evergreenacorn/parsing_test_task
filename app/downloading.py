11beba6917b082354a18b54068fd38b66e3fd29c
evergreenacorn <rasakjustone01.11@gmail.com>
2021-08-12 11:54:06 +0300

    Добавлено консольное меню приложения.

--- app/downloading.py
+++ app/downloading.py
@@ -99,3 +99,64 @@ def run(filename, urls_dict):
     # clear_extracted_yml_files(Config.BASE_DIR)
 
     print('Finished at: %s' % (time.time() - start_time), end="\n")
+
+
+def circle_run(filename, urls_dict):
+    filename = uzip_yml(Config.GZ_FILE)
+    read_file = get_document_from_file(filename)
+    menu_dict = {
+        1: {
+            'select': 'Вывод на печать всех объектов категорий.',
+            'return': 'Все объекты категорий были выведены.'
+        },
+        2: {
+            'select': 'Вывод на печать первых тысячи объектов офферов.',
+            'return': 'Первые тысяча объектов офферов были выведены на печать.'
+        },
+        3: {
+            'select': 'Скачивание картинок первых тысячи объектов офферов,' +
+            'с последующим добавлением ссылок на них в файл.',
+            'return': 'Скачивание картинок первых тысячи объектов офферов,' +
+            'с последующим добавлением ссылок на них в файл завершено.'
+        },
+        4: {
+            'select': 'Очистить все файлы текущей сессии программы.',
+            'return': 'Очистка всех файлов текущей сессии программы завершена.'
+        },
+        0: {
+            'select': 'Выход из программы',
+            'return': 'Завершение программы...'
+        }
+    }
+
+    while 1:
+        print('Вы находитесь в главном меню программы.\n')
+        print(
+            'Вам доступны следующие действия:\n',
+            *[menu_dict[x]['select'] for x in menu_dict],
+            sep='\n', end='\n')
+        selected = int(input("Выберите действие...\n",))
+        print(
+            "Вы выбрали: {}\n".format(
+                menu_dict[selected]['select']
+            ) if selected in menu_dict.keys() else
+            "Ошибка считывания выбранного действия...\n"
+        )
+        if selected == 0:
+            print(menu_dict[selected]['return'])
+            exit()
+        elif selected == 1:
+            print(menu_dict[selected]['return'])
+            print_all_cat_objects(read_file)
+        elif selected == 2:
+            print(menu_dict[selected]['return'])
+            print_first_thousand_offer_objects(read_file, 1000)
+        elif selected == 3:
+            print(menu_dict[selected]['return'])
+            offers_list = _get_first_thousand_offer_objects(read_file, 1000)
+            images_list = get_first_thousand_offer_img_urls(offers_list)
+            main(images_list, urls_dict)
+            rewrote_offers_images_path(read_file, urls_dict, filename, 1000)
+        elif selected == 4:
+            print(menu_dict[selected]['return'])
+            clear_extracted_yml_files(Config.BASE_DIR)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from downloading import  run, Config
+from downloading import  run, Config, circle_run
 # from config import Config
 
 
@@ -6,5 +6,9 @@ current_file = None
 updated_urls_dict = {}
 
 
+def run_program(current_file, updated_urls_dict):
+    circle_run(current_file, updated_urls_dict)
+
+
 if __name__ == '__main__':
-    run(current_file, updated_urls_dict)
+    run_program(current_file, updated_urls_dict)
